d96663599286c333b412d1d52c8b516ee6f0a078
Jordan Dalton <jordandalton@wrsgroup.com>
2014-09-10 11:22:30 -0500

    COnverted indents to spaces.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,36 +12,36 @@ use Illuminate\Routing\Controller;
 
 class AuthController extends Controller {
 
-	/**
-	 * The authenticator implementation.
-	 *
-	 * @var Authenticator
-	 */
-	protected $auth;
+    /**
+     * The authenticator implementation.
+     *
+     * @var Authenticator
+     */
+    protected $auth;
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  Authenticator  $auth
-	 * @return void
-	 */
-	public function __construct(Authenticator $auth)
-	{
-		$this->auth = $auth;
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param  Authenticator  $auth
+     * @return void
+     */
+    public function __construct(Authenticator $auth)
+    {
+        $this->auth = $auth;
 
-		$this->beforeFilter('csrf', ['on' => ['post']]);
-		$this->beforeFilter('guest', ['except' => ['getLogout']]);
-	}
+        $this->beforeFilter('csrf', ['on' => ['post']]);
+        $this->beforeFilter('guest', ['except' => ['getLogout']]);
+    }
 
-	/**
-	 * Show the application registration form.
-	 *
-	 * @return Response
-	 */
-	public function getRegister()
-	{
-		return view('auth.register');
-	}
+    /**
+     * Show the application registration form.
+     *
+     * @return Response
+     */
+    public function getRegister()
+    {
+        return view('auth.register');
+    }
 
     /**
      * Handle a registration request for the application.
@@ -51,45 +51,45 @@ class AuthController extends Controller {
      *
      * @return Response
      */
-	public function postRegister(RegisterRequest $request, UserRepository $repository )
-	{
-		// Create a new user record.
+    public function postRegister(RegisterRequest $request, UserRepository $repository )
+    {
+        // Create a new user record.
         //
         $user = $repository->create( $request->get('email'), $request->get('passsord') );
 
         // Log the user into their account.
         //
-		$this->auth->login( $user );
+        $this->auth->login( $user );
 
         // Redirect the user.
         //
-		return redirect('/');
-	}
+        return redirect('/');
+    }
 
-	/**
-	 * Show the application login form.
-	 *
-	 * @return Response
-	 */
-	public function getLogin()
-	{
-		return view('auth.login');
-	}
+    /**
+     * Show the application login form.
+     *
+     * @return Response
+     */
+    public function getLogin()
+    {
+        return view('auth.login');
+    }
 
-	/**
-	 * Handle a login request to the application.
-	 *
-	 * @param  LoginRequest  $request
-	 * @return Response
-	 */
-	public function postLogin(LoginRequest $request)
-	{
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  LoginRequest  $request
+     * @return Response
+     */
+    public function postLogin(LoginRequest $request)
+    {
         // If authentication is successful lets send them to the homepage.
         //
-		if ($this->auth->attempt($request->only('email', 'password')))
-		{
-			return redirect('/');
-		}
+        if ($this->auth->attempt($request->only('email', 'password')))
+        {
+            return redirect('/');
+        }
 
         // Flash an error message to the user.
         //
@@ -97,19 +97,19 @@ class AuthController extends Controller {
 
         // Redirect the user back to the page.
         //
-		return redirect('/login')->withInput();
-	}
+        return redirect('/login')->withInput();
+    }
 
-	/**
-	 * Log the user out of the application.
-	 *
-	 * @return Response
-	 */
-	public function getLogout()
-	{
-		$this->auth->logout();
+    /**
+     * Log the user out of the application.
+     *
+     * @return Response
+     */
+    public function getLogout()
+    {
+        $this->auth->logout();
 
-		return redirect('/');
-	}
+        return redirect('/');
+    }
 
 }
